f4c04dab8671ed701134a12f95c204857b68420c
unknown <buinguyentruc2003tb@mail.com>
2024-08-29 13:59:30 +0700

    update them newscreen

---
+++ lib/widgets/expenses_list/newscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewScreen extends StatelessWidget {
+  const NewScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
