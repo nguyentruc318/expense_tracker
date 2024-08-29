f8eb21bffc45873b94163cb5029256ce84dce9ff
unknown <buinguyentruc2003tb@mail.com>
2024-08-29 16:35:44 +0700

    nhanh-2

--- lib/widgets/expenses_list/newscreen.dart
+++ lib/widgets/expenses_list/newscreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class NewScreen extends StatelessWidget {
+  //ajdjskads
   const NewScreen({super.key});
   @override
   Widget build(BuildContext context) {
